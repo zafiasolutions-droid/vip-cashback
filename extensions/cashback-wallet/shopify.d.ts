0cc4533a72c0786d198c88d1933238c9d8ef1503
Apna GitHub username <Apni GitHub email>
2026-08-23 10:37:55 +0500

    Update cashback redemption logic

--- extensions/cashback-wallet/shopify.d.ts
+++
@@ -1,7 +0,0 @@
-import '@shopify/ui-extensions';
-
-//@ts-ignore
-declare module './src/OrderStatusBlock.jsx' {
-  const shopify: import('@shopify/ui-extensions/customer-account.page.render').Api;
-  const globalThis: { shopify: typeof shopify };
-}
