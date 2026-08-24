c9471a8b5ed26ebf38578378e995ee059498cfd8
zafiasolutions-droid <zafiasolutions@gmail.com>
2026-08-24 15:18:59 +0500

    Update cashback redemption logic

--- app/services/vip-status.server.js
+++ app/services/vip-status.server.js
@@ -152,6 +152,12 @@ export async function isCustomerVip({
     shopId,
     shopifyCustomerId,
   });
-
+console.log("VIP STATUS CHECK", {
+  shopifyCustomerId,
+  manualVip,
+  spendingVip,
+  twitchVip,
+  reasons,
+});
   return status.isVip;
 }
